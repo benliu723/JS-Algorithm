f119244e4993435bace82176bfa293e7750e4031
jiangweiliu <jiangweiliu@appannie.com>
2018-04-03 16:33:41 +0800

    added lint staged

--- src/dataStructures/heap/index.js
+++ src/dataStructures/heap/index.js
@@ -3,4 +3,6 @@ const parent = (i: number): number => Math.floor(i / 2);
 const left = (i: number): number => 2 * i;
 const right = (i: number): number => 2 * i + 1;
 
+var a = 1;
+
 export { parent, left, right };
