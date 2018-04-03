6d0eeacce51c2e960da46a376f20e4cc870eaba0
jiangweiliu <jiangweiliu@appannie.com>
2018-04-03 16:55:46 +0800

    test

--- src/dataStructures/heap/index.js
+++ src/dataStructures/heap/index.js
@@ -1,9 +1,7 @@
 // @flow
+
 const parent = (i: number): number => Math.floor(i / 2);
 const left = (i: number): number => 2 * i;
 const right = (i: number): number => 2 * i + 1;
-
-var a = 1;
-var a = 2;
 
 export { parent, left, right };
