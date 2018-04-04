d4b5d9f60f18bdfe9f0bf3f23e6aef20aea727b4
jiangweiliu <jiangweiliu@appannie.com>
2018-04-04 14:26:00 +0800

    added heap sort

--- src/dataStructures/sort/heapSort/index.js
+++ src/dataStructures/sort/heapSort/index.js
@@ -1,30 +1,25 @@
 // @flow
-import {
-    buildMaxHeap,
-    buildMinHeap,
-    maxHeapify,
-    minHeapify,
-} from '../dataStructures/heap';
-import * as utils from '../utils';
+import { buildMaxHeap, buildMinHeap, maxHeapify, minHeapify } from '../../heap';
+import * as utils from '../../../utils';
 
 export const heapSort = (arr: Array<number>) => {
-    const heap = buildMinHeap(arr);
-    let i = heap.heapSize - 1;
-    while (i > 0) {
-        utils.swap(arr, 0, i);
-        heap.heapSize -= 1;
-        minHeapify(heap, 0);
-        i -= 1;
-    }
-};
-
-export const reverseHeapSort = (arr: Array<number>) => {
     const heap = buildMaxHeap(arr);
     let i = heap.heapSize - 1;
     while (i > 0) {
         utils.swap(arr, 0, i);
         heap.heapSize -= 1;
         maxHeapify(heap, 0);
+        i -= 1;
+    }
+};
+
+export const reverseHeapSort = (arr: Array<number>) => {
+    const heap = buildMinHeap(arr);
+    let i = heap.heapSize - 1;
+    while (i > 0) {
+        utils.swap(arr, 0, i);
+        heap.heapSize -= 1;
+        minHeapify(heap, 0);
         i -= 1;
     }
 };
