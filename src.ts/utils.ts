2d6c3cfc816364a04c422e060a6661e4090ead9d
benliu723 <benliu723@gmail.com>
2021-03-31 11:40:04 +0800

    format code

--- src.ts/utils.ts
+++ src.ts/utils.ts
@@ -1,16 +1,19 @@
-export const swap = <T>(list:Array<T>, i: number, j: number) => {
+export const swap = <T>(list: Array<T>, i: number, j: number) => {
     const temp = list[i];
-    list[i] = list[j]
-    list[j]= temp;
-}
+    list[i] = list[j];
+    list[j] = temp;
+};
 
-export const random = (len: number, factor: number): Array<number> =>{
+export const random = (len: number, factor: number): Array<number> => {
     return Array(len)
         .fill(1)
-        .map(() => Math.round(Math.random() * factor))
-}
-    
-export const isSorted = <T>(list: Array<T>, comparator: (item1: T, item2: T) => boolean): boolean => {
+        .map(() => Math.round(Math.random() * factor));
+};
+
+export const isSorted = <T>(
+    list: Array<T>,
+    comparator: (item1: T, item2: T) => boolean
+): boolean => {
     let flag = true;
     let i = 0;
     while (i < list.length - 1) {
